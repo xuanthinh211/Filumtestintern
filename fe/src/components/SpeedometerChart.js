13ddf677d4946c4f1f7f0a7f13a94dcd1b30f0c4
Nguyễn Xuân Thịnh <nguyenxuanthinh345@gmail.com>
2024-11-10 19:38:10 +0700

    1

--- fe/src/components/SpeedometerChart.js
+++ fe/src/components/SpeedometerChart.js
@@ -2,85 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ReactSpeedometer from 'react-d3-speedometer';
-import "../styles/SpeedometerChart.css"
-const levels = [
-  {
-    level: 1,
-    icon: "https://assets.filum.ai/assessments/voc-level1.svg",
-    name: "Sơ khai",
-    range: [0, 2],
-    description: "Nguồn dữ liệu chính về phản hồi khách hàng đến từ các khảo sát theo năm hoặc không đều đặn. Các cuộc khảo sát được thực hiện độc lập bởi các phòng ban khác nhau mà không có sự chia sẻ kết quả và không lưu trữ tập trung. Hiếm khi thu thập phản hồi gián tiếp (từ bản ghi cuộc gọi, tin nhắn, bình luận v.v) hoặc phản hồi được suy luận từ hành vi, tần suất hay thói quen mua hàng của khách. Các quyết định từ đó kém hiệu quả khi dựa nhiều vào phản hồi đã cũ và không đáng tin cậy.",
-    key_actions: [
-      "Hình thành một đội nhóm liên phòng ban để quản lý quy trình thu thập và phân tích phản hồi khách hàng.",
-      "Định kỳ tổ chức phỏng vấn khách hàng để thu thập thông tin chi tiết.",
-      "Xây dựng cơ sở dữ liệu tập trung lưu trữ phản hồi khách hàng.",
-      "Xác định và thực hiện phân tích nguyên nhân gốc rễ từ phản hồi khách hàng.",
-      "Tạo ra cơ chế thông báo kết quả phản hồi cho các đại diện phòng ban liên quan."
-    ]
-  },
-  {
-    level: 2,
-    icon: "https://assets.filum.ai/assessments/voc-level2.svg",
-    name: "Thành lập",
-    range: [2, 4],
-    description: "Có đội nhóm liên phòng ban để đánh giá và điều phối việc thu thập phản hồi, phân tích nguyên nhân gốc rễ và thông báo kết quả đến các đại diện. Bước đầu quản trị năng lực Lắng nghe khách hàng để giải quyết vấn đề phân mảnh và thiếu phối hợp, tuy nhiên vấn đề thực thi còn yếu. Bắt đầu có các cuộc khảo sát tại điểm chạm nhưng chưa phải tất cả các điểm chạm quan trọng.",
-    key_actions: [
-      "Xây dựng một hệ thống thu thập phản hồi và lắng nghe khách hàng hiệu quả.",
-      "Đảm bảo tích hợp dữ liệu phản hồi trực tiếp và gián tiếp từ nhiều nguồn khác nhau.",
-      "Thực hiện quy trình đóng vòng phản hồi để xử lý các khiếu nại và vấn đề của khách hàng.",
-      "Mở rộng việc thực hiện khảo sát tại tất cả các điểm chạm quan trọng, xuyên suốt các giai đoạn của hành trình khách hàng.",
-      "Phát triển các tiêu chí và chỉ số để đo lường hiệu quả của việc Lắng nghe khách hàng."
-    ]
-  },
-  {
-    level: 3,
-    icon: "https://assets.filum.ai/assessments/voc-level3.svg",
-    name: "Vận hành",
-    range: [4, 6],
-    description: "Quản trị năng lực Lắng nghe khách hàng đã hình thành và đem lại hiệu quả. Dữ liệu phản hồi trực tiếp từ khách hàng được kết hợp với dữ liệu từ các nguồn khác (Ví dụ: từ điểm chạm web, cửa hàng, v.v.) và phản hồi gián tiếp (Ví dụ: bản ghi cuộc gọi, tin nhắn, bình luận mạng xã hội v.v). Quy trình đóng vòng phản hồi giúp xác định và giải quyết các khiếu nại hoặc vấn đề khách hàng gặp phải. Phản hồi được thu thập trong nhiều giai đoạn của hành trình khách hàng.",
-    key_actions: [
-      "Xây dựng năng lực Lắng nghe khách hàng tập trung trên một nền tảng, với quy trình và quy định rõ ràng về trách nhiệm và thực thi.",
-      "Ban hành các quy trình, biểu mẫu về Lắng nghe khách hàng rộng rãi cho toàn thể công ty.",
-      "Tạo ra các báo cáo và bảng điều khiển với dữ liệu phản hồi theo thời gian thực cho nhân viên và phòng ban.",
-      "Tích hợp phản hồi khách hàng vào các ứng dụng quan trọng như CRM, giúp ích vào hoạt động hằng ngày của nhân viên."
-    ]
-  },
-  {
-    level: 4,
-    icon: "https://assets.filum.ai/assessments/voc-level4.svg",
-    name: "Tối ưu",
-    range: [6, 8],
-    description: "Năng lực Lắng nghe khách hàng hoạt động đầy đủ trên một nền tảng duy nhất với quy định về trách nhiệm và thực thi rõ ràng. Các quy trình làm việc, báo cáo và phân tích được thông báo đầy đủ và tường minh, cung cấp dữ liệu thời gian thực, phù hợp cho từng nhân viên và phòng ban. Thông tin về phản hồi được tích hợp vào các ứng dụng quan trọng và phổ biến như CRM.",
-    key_actions: [
-      "Ứng dụng công nghệ tự động dựa trên dữ liệu lớn và AI giúp phân tích và đưa ra hành động khuyến nghị từ phản hồi khách hàng.",
-      "Định lượng giá trị của Lắng nghe khách hàng thông qua kết nối các hành động khuyến nghị và kết quả thu được lên các chỉ số liên quan đến vận hành và khách hàng.",
-      "Xây dựng cơ chế cho việc thu thập ý kiến phản hồi từ nhân viên nhằm cải thiện trải nghiệm khách hàng.",
-      "Tổ chức đều đặn các hoạt động khuyến khích nhân viên tham gia đóng góp ý kiến để cải thiện trải nghiệm khách hàng."
-    ]
-  },
-  {
-    level: 5,
-    icon: "https://assets.filum.ai/assessments/voc-level5.svg",
-    name: "Thấm nhuần",
-    range: [8, 11],
-    description: "Hiệu quả của Lắng nghe khách hàng được định lượng bằng việc kết nối các hành động đề xuất với kết quả thu được lên các chỉ số liên quan đến vận hành và khách hàng. Có cơ chế liên tục để thu thập ý kiến phản hồi từ nhân viên nhằm cải thiện trải nghiệm khách hàng.",
-    key_actions: [
-      "Duy trì một hệ thống Lắng nghe khách hàng đồng nhất và liên tục.",
-      "Đầu tư vào công nghệ và công cụ hiện đại về dữ liệu lớn và AI để hỗ trợ việc ra quyết định hiệu quả hơn.",
-      "Tạo dựng văn hóa doanh nghiệp tập trung vào cải tiến liên tục trải nghiệm khách hàng.",
-      "Phát triển các chương trình đào tạo và huấn luyện nhằm nâng cao nhận thức về năng lực Lắng nghe khách hàng cho nhân viên.",
-      "Duy trì cơ chế liên tục thu thập ý kiến phản hồi từ nhân viên và khuyến khích sự đóng góp của họ vào việc cải thiện trải nghiệm khách hàng."
-    ]
-  }
-];
-
-const images = {
-  "Sơ khai": "/thumbnails/level_1.png",
-  "Phát triển": "/thumbnails/level_2.png",
-  "Trưởng thành": "/thumbnails/level_3.png",
-  "Chuyên nghiệp": "/thumbnails/level_4.png",
-  "Xuất sắc": "/thumbnails/level_5.png"
-};
+import "../styles/SpeedometerChart.css";
+import assessmentData from '../data/assessment.json';
 
 const SpeedometerChart = () => {
   const [result, setResult] = useState(null);
@@ -88,10 +11,15 @@ const SpeedometerChart = () => {
   const [showEmailForm, setShowEmailForm] = useState(false);
   const [email, setEmail] = useState('');
 
+  const getMaturityLevelDetails = (score) => {
+    return assessmentData.results.find(result => score >= result.range[0] && score < result.range[1]);
+  };
+
   useEffect(() => {
     const fetchResult = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/result.json');
+        const response = await axios.get('https://filumtestinternbe.onrender.com/result.json');
+        console.log('Fetched result:', response.data); // Log dữ liệu nhận được
         setResult(response.data);
       } catch (error) {
         console.error('Error fetching result:', error);
@@ -154,14 +82,14 @@ const SpeedometerChart = () => {
 
   const handleFacebookShare = () => {
     const { maturityLevel } = result;
-    const imageUrl = images[maturityLevel];
+    const imageUrl = assessmentData.results.find(level => level.name === maturityLevel).icon;
 
     FB.ui({
       method: 'share',
-      href: 'http://localhost:3000', // Replace with your localhost URL
+      href: 'https://filumtestintern.vercel.app', // Replace with your frontend URL
       hashtag: '#YourHashtag',
       quote: 'Check out my maturity level!',
-      picture: `http://localhost:3000${imageUrl}` // Replace with the correct path to your images
+      picture: imageUrl // Replace with the correct path to your images
     }, function(response){});
   };
 
@@ -170,27 +98,27 @@ const SpeedometerChart = () => {
   }
 
   const { totalScore, maturityLevel } = result;
-  const levelInfo = levels.find(level => level.name === maturityLevel);
+  const maturityDetails = getMaturityLevelDetails(totalScore);
 
   return (
     <div className='chart-container'>
       <div className='chart-box'>
         <h2>Kết quả đánh giá</h2>
         <div className='chart-result'>
-          {levelInfo && (
+          {maturityDetails && (
             <div className="level-info">
               <div className="chart-result-name">
-                <img src={levelInfo.icon} alt={levelInfo.name} />
-                <h3>{levelInfo.name}</h3>
+                <img src={maturityDetails.icon} alt={maturityDetails.name} />
+                <h3>{maturityDetails.name}</h3>
               </div>
               <div className='chart-desc'>
-                <p>{levelInfo.description}</p>
+                <p>{maturityDetails.description}</p>
                 <h4>Hành động chính:</h4>
               </div>
               <div className='chart-desc'>
                 <ul>
-                  {levelInfo.key_actions.map((action, index) => (
-                    <li key={index}>{action}</li>
+                  {maturityDetails.key_actions.map((action, index) => (
+                    <li key={index}>{action.text}</li>
                   ))}
                 </ul>
               </div>
